ded9c34955cdaf78cb942bf0bc29454e17bc5bcf
rabeahmed2002 <mrabe2024@gmail.com>
2025-01-20 21:50:53 +0500

    counter II optimized

--- 2665. Counter II/index.js
+++ 2665. Counter II/index.js
@@ -1,18 +1,9 @@
 var createCounter = function(init) {
     let presentCount=init;
 
-    function increment() {
-        return ++presentCount;
-    }
-
-    function decrement() {
-        return --presentCount;
-    }
-
-    function reset() {
-        return (presentCount=init)
-    }
-
-    return {increment, decrement, reset}
-    
+    return {
+        increment: () => ++presentCount,
+        decrement: () => --presentCount,
+        reset: () => (presentCount = init)
+    };
 };
